04faa36be654f8601ef8a3b0402dd09e0f39454a
cetinogut <cetinogut.gm@gmail.com>
2022-07-31 23:19:26 +0300

    Add project files.

---
+++ EmployeeManagementManila/Extensions/ServiceExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementManila.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader());
+            });
+        }
+        // CORS(Cross-Origin Resource Sharing) is a mechanism that gives rights to the user to access resources from the server on a different domain.Because we will use Angular as a client-side on a different domain than the server’s domain, configuring CORS is mandatory.
+
+        public static void ConfigureIISIntegration(this IServiceCollection services)
+        {
+            services.Configure<IISOptions>(options =>
+            {
+
+            });
+        }
+    }
+}
